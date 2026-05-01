7a3cd946ef550250fd52de06db602d38eea21096
Mahedi <mahidehasansiyam123@gmail.com>
2026-05-01 09:07:13 +0600

    Navbar

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,27 +1,25 @@
-// import { getData } from '@/lib/DataFetch';
-// import React from 'react';
-// import ShowTiles from '../components/ShowTiles';
+import { getData } from '@/lib/DataFetch';
+import React from 'react';
+import ShowTiles from '../components/ShowTiles';
 
 const AllTiles =async () => {
 
-  // const AllData = await getData();
-  //   // console.log(AllData);
+  const AllData = await getData();
+    // console.log(AllData);
   return (
-    // <div className="max-w-7xl mx-auto" >
-    //   <h2 className="font-bold text-2xl">All Tiles:</h2>
-    //   <div className="grid grid-cols-4 gap-4">
-    //     {AllData.map(tiles => {
-    //       return (
-    //         <div key={tiles.id}>
-    //           <ShowTiles tiles={tiles}></ShowTiles>
-    //         </div>
-    //       );
-    //     })}
-    //   </div>
-    // </div>
-    <div>
-      fkjg
+    <div className="max-w-7xl mx-auto" >
+      <h2 className="font-bold text-2xl">All Tiles:</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {AllData.map(tiles => {
+          return (
+            <div key={tiles.id}>
+              <ShowTiles tiles={tiles}></ShowTiles>
+            </div>
+          );
+        })}
+      </div>
     </div>
+   
   );
 };
 
