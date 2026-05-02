349deeb674bda6dfc547a6510aa796c018948912
Mahedi <mahidehasansiyam123@gmail.com>
2026-05-02 16:44:50 +0600

    use loader

---
+++ src/app/tiles/[id]/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <span className="loading loading-infinity loading-xl"></span>
+      
+    </div>
+  );
+};
+
+export default loading;
